41354e44d6ac667a9eb25545de94119f73ab32be
Lucas Lima <lukkasoliveiralima@gmail.com>
2022-05-31 16:29:35 -0300

    abstraindo contexto

--- application/src/Shared/Infrastructure/Message/Producer.php
+++ application/src/Shared/Infrastructure/Message/Producer.php
@@ -4,26 +4,16 @@ namespace EcommercePhp\Shared\Infrastructure\Message;
 
 use EcommercePhp\Shared\Domain\Message\Message;
 use EcommercePhp\Shared\Domain\Message\Producer as ProducerInterface;
-use Enqueue\RdKafka\RdKafkaConnectionFactory;
 
 class Producer implements ProducerInterface
 {
+    public function __construct(private ContextManager $contextManager)
+    {
+    }
+
     public function produce(Message $message): void
     {
-        $config = [
-            'global' => [
-                'group.id' => uniqid('group_id', true),
-                'metadata.broker.list' => env('KAFKA_BROKERS'),
-                'enable.auto.commit' => 'false',
-            ],
-            'topic' => [
-                'auto.offset.reset' => 'beginning',
-            ],
-        ];
-
-        $factory = new RdKafkaConnectionFactory($config);
-        $context = $factory->createContext();
-
+        $context = $this->contextManager->getContext();
         $topic = $context->createTopic($message->getTopic());
 
         $queueMessage = $context->createMessage(
