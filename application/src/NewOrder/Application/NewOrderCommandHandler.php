5ea7225cb5e9d60e0840534e88f641d0bf17540e
Lucas Lima <lukkasoliveiralima@gmail.com>
2022-05-31 15:16:28 -0300

    produzindo primeira mensagem

--- application/src/NewOrder/Application/NewOrderCommandHandler.php
+++ application/src/NewOrder/Application/NewOrderCommandHandler.php
@@ -5,6 +5,8 @@ namespace EcommercePhp\NewOrder\Application;
 use EcommercePhp\NewOrder\Domain\Validator\PayloadValidator;
 use EcommercePhp\Shared\Domain\Bus\Command;
 use EcommercePhp\Shared\Domain\Bus\CommandHandler;
+use Enqueue\RdKafka\RdKafkaConnectionFactory;
+use Illuminate\Support\Str;
 
 class NewOrderCommandHandler implements CommandHandler
 {
@@ -16,8 +18,37 @@ class NewOrderCommandHandler implements CommandHandler
     {
         $this->validator->validate($command->toArray());
 
+        $config = [
+            'global' => [
+                'group.id' => uniqid('group_id', true),
+                'metadata.broker.list' => env('KAFKA_BROKERS'),
+                'enable.auto.commit' => 'false',
+            ],
+            'topic' => [
+                'auto.offset.reset' => 'beginning',
+            ],
+        ];
+
+        $factory = new RdKafkaConnectionFactory($config);
+        $context = $factory->createContext();
+
+        $topic = $context->createTopic('example-topic');
+
+        $message = $context->createMessage(
+            json_encode($command->toArray()),
+            [],
+            []
+        );
+
+        $message->setKey(Str::uuid()->toString());
+        //$message->setKey('minha-key');
+
+        $producer = $context->createProducer();
+
+        $producer->send($topic, $message);
+
         return [
-            'payload' => $command->toArray(),
+            'message' => 'Obrigado pelo pedido',
         ];
     }
 }
