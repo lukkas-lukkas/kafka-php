7258299994b2cb6e47d2d25c731ae79fd42cde96
Lucas Lima <lukkasoliveiralima@gmail.com>
2022-05-31 17:33:59 -0300

    abstraindo consumidor

---
+++ application/src/Shared/UI/Console/AbstractProcessorCommand.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace EcommercePhp\Shared\UI\Console;
+
+use EcommercePhp\Shared\Infrastructure\Message\ContextManager;
+use Enqueue\Consumption\QueueConsumer;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
+use Interop\Queue\Context;
+use Interop\Queue\Message;
+use Interop\Queue\Processor;
+
+abstract class AbstractProcessorCommand extends Command implements Processor
+{
+    public function __construct(private ContextManager $contextManager)
+    {
+        parent::__construct();
+    }
+
+    public function __invoke(): void
+    {
+        $this->setGroupId();
+        $context = $this->contextManager->getContext();
+
+        $queueConsumer = new QueueConsumer($context);
+
+        $queueConsumer->bind($this->getTopic(), $this);
+
+        $queueConsumer->consume();
+    }
+
+    public function process(Message $message, Context $context): string
+    {
+        try {
+            $this->consume($message, $context);
+            return self::ACK;
+        } catch (\Throwable $exception) {
+            Log::info('process-new-order-error', [
+                'message' => $exception->getMessage(),
+                'file' => $exception->getFile(),
+                'exception' => $exception
+            ]);
+
+            //Poderiamos tbm mandar a mensagem para uma DLQ
+            return self::REJECT;
+        }
+    }
+
+    private function setGroupId(): void
+    {
+        $groupId = (new \ReflectionClass($this))->getShortName();
+
+        $this->contextManager->setGroupId($groupId);
+    }
+
+    abstract protected function getTopic(): string;
+
+    abstract protected function consume(Message $message, Context $context): void;
+}
